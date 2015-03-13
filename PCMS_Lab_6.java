ddaa5b318f8761d15eb1255904394f7289ebffb1
Hiral-panchal <hiral.panchal@iet.ahduni.edu.in>
2015-03-13 20:02:36 +0530

    Lab_6

---
+++ PCMS_Lab_6.java
@@ -0,0 +1,84 @@
+public class PCMS 
+{
+
+static Monitor monitor;
+
+public PCMS(int maxSize)
+{
+	monitor = new Monitor(maxSize);
+	new Producer().start();
+	new Producer().start();
+	new Consumer().start();
+	new Consumer().start();
+}
+
+class Producer extends Thread
+{
+
+public void run() 
+	{
+		while(true)
+		{
+				try {
+					monitor.insert();
+				} catch (InterruptedException e) {
+				e.printStackTrace();
+				}
+         }
+     }
+ }
+
+class Consumer extends Thread
+{
+
+
+public void run() 
+		{
+			while(true)
+            {
+				try {
+				monitor.remove();
+				} catch (InterruptedException e) {
+				e.printStackTrace();
+                }
+            }
+        }
+ }
+
+class Monitor 
+{
+
+int n;
+int maxSize;
+
+public Monitor(int maxSize)
+         {
+             n=0;
+			this.maxSize = maxSize;
+         }
+
+synchronized void insert() throws InterruptedException
+        {
+			if(n==maxSize)
+			wait();
+			System.out.println("Producer: "+n++);
+			if(n==1)
+			notifyAll();
+        }
+
+synchronized void remove() throws InterruptedException
+        {
+			if(n==0)
+				wait();
+			System.out.println("Consumer: "+n--);
+			if(n==maxSize-1)
+				notifyAll();
+        }
+}
+
+public static void main(String[] args) 
+	{
+		PCMS pc  = new PCMS(5);
+
+    }
+}
